fc763c33c1c6fee2732278f4cf82a1489c66f221
leeyiyuan <leeyiyuan@hotmail.com>
2019-07-03 15:22:20 +0800

    Add initial rendering of lanes

--- Unreal/CarlaUE4/Plugins/Carla/Source/OSMImport/LaneNetworkActor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/OSMImport/LaneNetworkActor.cpp
@@ -25,4 +25,74 @@ void ALaneNetworkActor::SetLaneNetwork(const FString& LaneNetworkPath) {
       LaneNetwork.Roads.Num(),
       LaneNetwork.Lanes.Num(),
       LaneNetwork.LaneConnections.Num());
+    
+  TArray<FVector> Vertices;
+  TArray<int> Triangles;
+
+  auto AddLineSegment = [&Vertices, &Triangles](FVector2D Start, FVector2D End, float Width) {
+    FVector2D Direction = End - Start;
+    Direction.Normalize();
+    FVector2D Normal = Direction.GetRotated(90);
+    
+    // Add line rectangle.
+    {
+      FVector2D V1 = Start + Normal * Width / 2; 
+      FVector2D V2 = Start - Normal * Width / 2; 
+      FVector2D V3 = End + Normal * Width / 2; 
+      FVector2D V4 = End - Normal * Width / 2; 
+
+      int VI1 = Vertices.Add(100 * FVector(V1.Y, V1.X, 0));
+      int VI2 = Vertices.Add(100 * FVector(V2.Y, V2.X, 0));
+      int VI3 = Vertices.Add(100 * FVector(V3.Y, V3.X, 0));
+      int VI4 = Vertices.Add(100 * FVector(V4.Y, V4.X, 0));
+
+      Triangles.Add(VI1);
+      Triangles.Add(VI2);
+      Triangles.Add(VI3);
+
+      Triangles.Add(VI4);
+      Triangles.Add(VI3);
+      Triangles.Add(VI2);
+    }
+ 
+    int N = 16;
+    // Add semicircles to start.
+    for (int I = 0; I < N; I++) {
+      FVector2D V1 = Start;
+      FVector2D V2 = Start + Normal.GetRotated((180.0f / N) * I) * Width / 2;
+      FVector2D V3 = Start + Normal.GetRotated((180.0f / N) * (I + 1)) * Width / 2;
+
+      int VI1 = Vertices.Add(100 * FVector(V1.Y, V1.X, 0));
+      int VI2 = Vertices.Add(100 * FVector(V2.Y, V2.X, 0));
+      int VI3 = Vertices.Add(100 * FVector(V3.Y, V3.X, 0));
+    
+      Triangles.Add(VI1);
+      Triangles.Add(VI2);
+      Triangles.Add(VI3);
+    }
+    // Add semicircles to end.
+    for (int I = 0; I < N; I++) {
+      FVector2D V1 = End;
+      FVector2D V2 = End + Normal.GetRotated(-(180.0f / N) * (I + 1)) * Width / 2;
+      FVector2D V3 = End + Normal.GetRotated(-(180.0f / N) * I) * Width / 2;
+
+      int VI1 = Vertices.Add(100 * FVector(V1.Y, V1.X, 0));
+      int VI2 = Vertices.Add(100 * FVector(V2.Y, V2.X, 0));
+      int VI3 = Vertices.Add(100 * FVector(V3.Y, V3.X, 0));
+      
+      Triangles.Add(VI1);
+      Triangles.Add(VI2);
+      Triangles.Add(VI3);
+    }
+  };
+
+  for (const auto& LaneEntry : LaneNetwork.Lanes) {
+    FVector2D Start = LaneNetwork.GetLaneStart(LaneEntry.Value);
+    FVector2D End = LaneNetwork.GetLaneEnd(LaneEntry.Value);
+    AddLineSegment(Start, End, LaneNetwork.LaneWidth);
+  }
+   
+
+  MeshComponent->CreateMeshSection_LinearColor(0, Vertices, Triangles, {}, {}, {}, {}, true);
+  MeshComponent->ContainsPhysicsTriMeshData(true);
 }
