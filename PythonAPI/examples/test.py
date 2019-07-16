346824345c461615b0c892f694d7298596590098
leeyiyuan <leeyiyuan@hotmail.com>
2019-07-16 09:36:36 +0800

    OpenCV contour tests

--- PythonAPI/examples/test.py
+++ PythonAPI/examples/test.py
@@ -19,20 +19,20 @@ if __name__ == '__main__':
     lane_network = carla.LaneNetwork.load('/home/leeyiyuan/Projects/osm-convert/network.ln')
     occupancy_map = lane_network.create_occupancy_map()
     
-    client = carla.Client('127.0.0.1', 2000)
-    client.set_timeout(2.0)
-    client.get_world().spawn_occupancy_map(occupancy_map)
+    #client = carla.Client('127.0.0.1', 2000)
+    #client.set_timeout(2.0)
+    #client.get_world().spawn_occupancy_map(occupancy_map)
 
     occupancy_grid = occupancy_map.create_occupancy_grid(
             carla.Vector2D(-500, -500),
             carla.Vector2D(500, 500),
             0.1)
     
-    img, contours, hierarchy = cv2.findContours(occupancy_grid.data, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    img, contours, hierarchy = cv2.findContours(
+            cv2.bitwise_not(occupancy_grid.data), 
+            cv2.RETR_EXTERNAL, 
+            cv2.CHAIN_APPROX_SIMPLE)
     
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
-    cv2.imwrite('test.bmp', occupancy_grid.data)
-
-    cv2.imshow('test', occupancy_grid.data)
-    cv2.waitKey(0)
+    cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
+    cv2.imwrite('test.bmp', img)
