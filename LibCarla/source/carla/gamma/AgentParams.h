fb325f4e47cebcd713d0ff372abaab5243ee99b4
luo-yuanfu <varellow@gmail.com>
2019-07-18 21:38:12 +0800

    fixed a bug in AgentParams.h

--- LibCarla/source/carla/gamma/AgentParams.h
+++ LibCarla/source/carla/gamma/AgentParams.h
@@ -1,45 +1,76 @@
-#ifndef DEFAULT_AGENT_PARAMS_H_
-#define DEFAULT_AGENT_PARAMS_H_
+#ifndef AGENT_PARAMS_H_
+#define AGENT_PARAMS_H_
 
 #include "Vector2.h"
 #include <string>
 #include <vector>
 #include <unordered_map>
 
-// namespace AgentTypeNs{
-// 	enum AgentType {
-// 		PEOPLE,
-// 		SCOOTER,
-// 		CAR,
-// 		VAN,
-// 		BUS,
-// 		JEEP,
-// 		BICYCLE,
-// 		ELECTRIC_TRICYCLE,
-// 		GYRO_SCOOTER,
-// 		AGENT_TYPE_COUNT	
-// 	};
-// };
-
 
 struct AgentParams{
 
-	RVO::Vector2  position = RVO::Vector2(0.0f, 0.0f);
-	float neighborDist = 8.0f;
-	int maxNeighbors = 5;
-	float timeHorizon = 3.0f;
-	float timeHorizonObst = 5.0f;
-	float radius = 0.25f;
-	float maxSpeed = 5.0f;
-	RVO::Vector2 velocity = RVO::Vector2(0.0f, 0.0f);
-	std::string tag = "People";
-	float max_tracking_angle = 90.0f;
-	float len_ref_to_front = 0.1f;
-	float len_ref_to_side = 0.22f;
-	float len_ref_to_back = 0.1f;
-	float len_rear_axle_to_front_axle = 0.2f;
-	float error_bound = 0.0f;
-	float pref_speed = 1.2f;
+	RVO::Vector2  position;
+	float neighborDist;
+	int maxNeighbors;
+	float timeHorizon;
+	float timeHorizonObst;
+	float radius;
+	float maxSpeed;
+	RVO::Vector2 velocity;
+	std::string tag;
+	float max_tracking_angle;
+	float len_ref_to_front;
+	float len_ref_to_side;
+	float len_ref_to_back;
+	float len_rear_axle_to_front_axle;
+	float error_bound;
+	float pref_speed;
+
+	AgentParams(){
+		position = RVO::Vector2(0.0f, 0.0f);
+		neighborDist = 8.0f;
+		maxNeighbors = 5;
+		timeHorizon = 3.0f;
+		timeHorizonObst = 5.0f;
+		radius = 0.25f;
+		maxSpeed = 5.0f;
+		velocity = RVO::Vector2(0.0f, 0.0f);
+		tag = "People";
+		max_tracking_angle = 90.0f;
+		len_ref_to_front = 0.1f;
+		len_ref_to_side = 0.22f;
+		len_ref_to_back = 0.1f;
+		len_rear_axle_to_front_axle = 0.2f;
+		error_bound = 0.0f;
+		pref_speed = 1.2f;
+	}
+
+
+	AgentParams(RVO::Vector2 _position, float _neighborDist, int _maxNeighbors,
+				float _timeHorizon, float _timeHorizonObst, float _radius, 
+				float _maxSpeed, RVO::Vector2 _velocity, std::string _tag,
+				float _max_tracking_angle, float _len_ref_to_front, float _len_ref_to_side,
+				float _len_ref_to_back, float _len_rear_axle_to_front_axle, float _error_bound, float _pref_speed){
+		position = _position;
+		neighborDist = _neighborDist;
+		maxNeighbors = _maxNeighbors;
+		timeHorizon = _timeHorizon;
+		timeHorizonObst = _timeHorizonObst;
+		radius = _radius;
+		maxSpeed = _maxSpeed;
+		velocity = _velocity;
+		tag = _tag;
+		max_tracking_angle = _max_tracking_angle;
+		len_ref_to_front = _len_ref_to_front;
+		len_ref_to_side = _len_ref_to_side;
+		len_ref_to_back = _len_ref_to_back;
+		len_rear_axle_to_front_axle = _len_rear_axle_to_front_axle;
+		error_bound = _error_bound;
+		pref_speed = _pref_speed;
+	}
+
+
+
 
 	static const AgentParams& getDefaultAgentParam(std::string tag){
 
@@ -64,8 +95,8 @@ struct AgentParams{
 			float ped_error_bound = 0.0f;
 			float ped_pref_speed = 1.2f;
 
-			AgentParams ped = {	ped_position, ped_neighborDist, ped_maxNeighbors, ped_timeHorizon, ped_timeHorizonObst, ped_radius, ped_maxSpeed, ped_velocity, ped_tag, 
-								ped_max_tracking_angle, ped_len_ref_to_front, ped_len_ref_to_side, ped_len_ref_to_back, ped_len_rear_axle_to_front_axle, ped_error_bound, ped_pref_speed};
+			AgentParams ped = AgentParams(	ped_position, ped_neighborDist, ped_maxNeighbors, ped_timeHorizon, ped_timeHorizonObst, ped_radius, ped_maxSpeed, ped_velocity, ped_tag, 
+								ped_max_tracking_angle, ped_len_ref_to_front, ped_len_ref_to_side, ped_len_ref_to_back, ped_len_rear_axle_to_front_axle, ped_error_bound, ped_pref_speed);
 			
 
 			lookup[ped_tag]=ped;
@@ -89,8 +120,8 @@ struct AgentParams{
 			float scooter_error_bound = 0.5f;
 			float scooter_pref_speed = 4.0f;
 
-			AgentParams scooter = {	scooter_position, scooter_neighborDist, scooter_maxNeighbors, scooter_timeHorizon, scooter_timeHorizonObst, scooter_radius, scooter_maxSpeed, scooter_velocity, scooter_tag, 
-						scooter_max_tracking_angle, scooter_len_ref_to_front, scooter_len_ref_to_side, scooter_len_ref_to_back, scooter_len_rear_axle_to_front_axle, scooter_error_bound, scooter_pref_speed};
+			AgentParams scooter = AgentParams(	scooter_position, scooter_neighborDist, scooter_maxNeighbors, scooter_timeHorizon, scooter_timeHorizonObst, scooter_radius, scooter_maxSpeed, scooter_velocity, scooter_tag, 
+						scooter_max_tracking_angle, scooter_len_ref_to_front, scooter_len_ref_to_side, scooter_len_ref_to_back, scooter_len_rear_axle_to_front_axle, scooter_error_bound, scooter_pref_speed);
 			
 			lookup[scooter_tag]=scooter;
 
@@ -112,8 +143,8 @@ struct AgentParams{
 			float car_error_bound = 0.5f;
 			float car_pref_speed = 4.0f;
 
-			AgentParams car = {	car_position, car_neighborDist, car_maxNeighbors, car_timeHorizon, car_timeHorizonObst, car_radius, car_maxSpeed, car_velocity, car_tag, 
-								car_max_tracking_angle, car_len_ref_to_front, car_len_ref_to_side, car_len_ref_to_back, car_len_rear_axle_to_front_axle, car_error_bound, car_pref_speed};
+			AgentParams car = AgentParams(	car_position, car_neighborDist, car_maxNeighbors, car_timeHorizon, car_timeHorizonObst, car_radius, car_maxSpeed, car_velocity, car_tag, 
+								car_max_tracking_angle, car_len_ref_to_front, car_len_ref_to_side, car_len_ref_to_back, car_len_rear_axle_to_front_axle, car_error_bound, car_pref_speed);
 			
 			lookup[car_tag]=car;
 
@@ -136,8 +167,8 @@ struct AgentParams{
 			float van_error_bound = 0.5f;
 			float van_pref_speed = 4.0f;
 
-			AgentParams van = {	van_position, van_neighborDist, van_maxNeighbors, van_timeHorizon, van_timeHorizonObst, van_radius, van_maxSpeed, van_velocity, van_tag, 
-								van_max_tracking_angle, van_len_ref_to_front, van_len_ref_to_side, van_len_ref_to_back, van_len_rear_axle_to_front_axle, van_error_bound, van_pref_speed};
+			AgentParams van = AgentParams(	van_position, van_neighborDist, van_maxNeighbors, van_timeHorizon, van_timeHorizonObst, van_radius, van_maxSpeed, van_velocity, van_tag, 
+								van_max_tracking_angle, van_len_ref_to_front, van_len_ref_to_side, van_len_ref_to_back, van_len_rear_axle_to_front_axle, van_error_bound, van_pref_speed);
 			
 			lookup[van_tag]=van;
 
@@ -160,8 +191,8 @@ struct AgentParams{
 			float bus_error_bound = 0.5f;
 			float bus_pref_speed = 4.0f;
 
-			AgentParams bus = {	bus_position, bus_neighborDist, bus_maxNeighbors, bus_timeHorizon, bus_timeHorizonObst, bus_radius, bus_maxSpeed, bus_velocity, bus_tag, 
-								bus_max_tracking_angle, bus_len_ref_to_front, bus_len_ref_to_side, bus_len_ref_to_back, bus_len_rear_axle_to_front_axle, bus_error_bound, bus_pref_speed};
+			AgentParams bus = AgentParams(	bus_position, bus_neighborDist, bus_maxNeighbors, bus_timeHorizon, bus_timeHorizonObst, bus_radius, bus_maxSpeed, bus_velocity, bus_tag, 
+								bus_max_tracking_angle, bus_len_ref_to_front, bus_len_ref_to_side, bus_len_ref_to_back, bus_len_rear_axle_to_front_axle, bus_error_bound, bus_pref_speed);
 			
 			lookup[bus_tag]=bus;
 
@@ -183,8 +214,8 @@ struct AgentParams{
 			float jeep_error_bound = 0.5f;
 			float jeep_pref_speed = 4.0f;
 
-			AgentParams jeep = { jeep_position, jeep_neighborDist, jeep_maxNeighbors, jeep_timeHorizon, jeep_timeHorizonObst, jeep_radius, jeep_maxSpeed, jeep_velocity, jeep_tag, 
-								jeep_max_tracking_angle, jeep_len_ref_to_front, jeep_len_ref_to_side, jeep_len_ref_to_back, jeep_len_rear_axle_to_front_axle, jeep_error_bound, jeep_pref_speed};
+			AgentParams jeep = AgentParams( jeep_position, jeep_neighborDist, jeep_maxNeighbors, jeep_timeHorizon, jeep_timeHorizonObst, jeep_radius, jeep_maxSpeed, jeep_velocity, jeep_tag, 
+								jeep_max_tracking_angle, jeep_len_ref_to_front, jeep_len_ref_to_side, jeep_len_ref_to_back, jeep_len_rear_axle_to_front_axle, jeep_error_bound, jeep_pref_speed);
 			
 			lookup[jeep_tag]=jeep;
 
@@ -207,8 +238,8 @@ struct AgentParams{
 			float bicycle_error_bound = 0.5f;
 			float bicycle_pref_speed = 2.7f;
 
-			AgentParams bicycle = {	bicycle_position, bicycle_neighborDist, bicycle_maxNeighbors, bicycle_timeHorizon, bicycle_timeHorizonObst, bicycle_radius, bicycle_maxSpeed, bicycle_velocity, bicycle_tag, 
-								bicycle_max_tracking_angle, bicycle_len_ref_to_front, bicycle_len_ref_to_side, bicycle_len_ref_to_back, bicycle_len_rear_axle_to_front_axle, bicycle_error_bound, bicycle_pref_speed};
+			AgentParams bicycle = AgentParams(	bicycle_position, bicycle_neighborDist, bicycle_maxNeighbors, bicycle_timeHorizon, bicycle_timeHorizonObst, bicycle_radius, bicycle_maxSpeed, bicycle_velocity, bicycle_tag, 
+								bicycle_max_tracking_angle, bicycle_len_ref_to_front, bicycle_len_ref_to_side, bicycle_len_ref_to_back, bicycle_len_rear_axle_to_front_axle, bicycle_error_bound, bicycle_pref_speed);
 			
 			lookup[bicycle_tag]=bicycle;
 
@@ -230,8 +261,8 @@ struct AgentParams{
 			float electric_tricycle_error_bound = 0.5f;
 			float electric_tricycle_pref_speed = 4.0f;
 
-			AgentParams electric_tricycle = {	electric_tricycle_position, electric_tricycle_neighborDist, electric_tricycle_maxNeighbors, electric_tricycle_timeHorizon, electric_tricycle_timeHorizonObst, electric_tricycle_radius, electric_tricycle_maxSpeed, electric_tricycle_velocity, electric_tricycle_tag, 
-								electric_tricycle_max_tracking_angle, electric_tricycle_len_ref_to_front, electric_tricycle_len_ref_to_side, electric_tricycle_len_ref_to_back, electric_tricycle_len_rear_axle_to_front_axle, electric_tricycle_error_bound, electric_tricycle_pref_speed};
+			AgentParams electric_tricycle = AgentParams(	electric_tricycle_position, electric_tricycle_neighborDist, electric_tricycle_maxNeighbors, electric_tricycle_timeHorizon, electric_tricycle_timeHorizonObst, electric_tricycle_radius, electric_tricycle_maxSpeed, electric_tricycle_velocity, electric_tricycle_tag, 
+								electric_tricycle_max_tracking_angle, electric_tricycle_len_ref_to_front, electric_tricycle_len_ref_to_side, electric_tricycle_len_ref_to_back, electric_tricycle_len_rear_axle_to_front_axle, electric_tricycle_error_bound, electric_tricycle_pref_speed);
 			
 			lookup[electric_tricycle_tag]=electric_tricycle;
 
@@ -254,8 +285,8 @@ struct AgentParams{
 			float gyro_scooter_error_bound = 0.1f;
 			float gyro_scooter_pref_speed = 2.0f;
 
-			AgentParams gyro_scooter = {	gyro_scooter_position, gyro_scooter_neighborDist, gyro_scooter_maxNeighbors, gyro_scooter_timeHorizon, gyro_scooter_timeHorizonObst, gyro_scooter_radius, gyro_scooter_maxSpeed, gyro_scooter_velocity, gyro_scooter_tag, 
-								gyro_scooter_max_tracking_angle, gyro_scooter_len_ref_to_front, gyro_scooter_len_ref_to_side, gyro_scooter_len_ref_to_back, gyro_scooter_len_rear_axle_to_front_axle, gyro_scooter_error_bound, gyro_scooter_pref_speed};
+			AgentParams gyro_scooter = AgentParams(	gyro_scooter_position, gyro_scooter_neighborDist, gyro_scooter_maxNeighbors, gyro_scooter_timeHorizon, gyro_scooter_timeHorizonObst, gyro_scooter_radius, gyro_scooter_maxSpeed, gyro_scooter_velocity, gyro_scooter_tag, 
+								gyro_scooter_max_tracking_angle, gyro_scooter_len_ref_to_front, gyro_scooter_len_ref_to_side, gyro_scooter_len_ref_to_back, gyro_scooter_len_rear_axle_to_front_axle, gyro_scooter_error_bound, gyro_scooter_pref_speed);
 			
 			lookup[gyro_scooter_tag]=gyro_scooter;
 
