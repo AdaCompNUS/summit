930b8531ecb1958e0de61ea6fc9e8d74ef885ef8
leeyiyuan <leeyiyuan@hotmail.com>
2019-08-23 22:59:37 +0800

    Add landmark methods to wrapper

--- PythonAPI/carla/source/libcarla/Landmark.cpp
+++ PythonAPI/carla/source/libcarla/Landmark.cpp
@@ -7,7 +7,15 @@ void export_landmark() {
   using namespace carla::landmark;
 
   class_<LandmarkMap>("LandmarkMap", no_init)
-    .def("load", &LandmarkMap::Load)
+    .def("load", 
+        +[](const std::string& file) { 
+          return LandmarkMap::Load(file); 
+        })
+    .def("load", 
+        +[](const std::string& file, const geom::Vector2D& offset) { 
+          return LandmarkMap::Load(file, offset); 
+        })
     .staticmethod("load")
+    .def("get_mesh_triangles", &LandmarkMap::GetMeshTriangles)
   ;
 }
