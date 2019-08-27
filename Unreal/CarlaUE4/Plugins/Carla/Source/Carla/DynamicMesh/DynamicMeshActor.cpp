6c51e73e87ba3f101fad399d8e9bc77dfa2a5bcd
leeyiyuan <leeyiyuan@hotmail.com>
2019-08-27 21:11:24 +0800

    Add UV and Tangents to DynamicMeshActor

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/DynamicMesh/DynamicMeshActor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/DynamicMesh/DynamicMeshActor.cpp
@@ -16,19 +16,43 @@ void ADynamicMeshActor::SetMaterial(const FString& Material) {
 void ADynamicMeshActor::SetTriangles(const TArray<FVector>& Triangles) {
   TArray<int> VertexIndices;
   VertexIndices.SetNum(Triangles.Num(), true);
-  for (int I = 0; I < Triangles.Num(); I++) {
-    VertexIndices[I] = I;
-  }
-
+  
   TArray<FVector> Normals;
   Normals.SetNum(Triangles.Num(), true);
+
+  TArray<FVector2D> UV;
+  UV.SetNum(Triangles.Num(), true);
+
+  TArray<FProcMeshTangent> Tangents;
+  Tangents.SetNum(Triangles.Num(), true);
+
   for (int I = 0; I < Triangles.Num(); I += 3) {
+    VertexIndices[I] = I;
+    VertexIndices[I + 1] = I + 1;
+    VertexIndices[I + 2] = I + 2;
+
     Normals[I] = FVector::CrossProduct(Triangles[I + 2] - Triangles[I], Triangles[I + 1] - Triangles[I]).GetSafeNormal();
     Normals[I + 1] = FVector::CrossProduct(Triangles[I] - Triangles[I + 1], Triangles[I + 2] - Triangles[I + 1]).GetSafeNormal();
     Normals[I + 2] = FVector::CrossProduct(Triangles[I + 1] - Triangles[I + 2], Triangles[I] - Triangles[I + 2]).GetSafeNormal();
+
+    UV[I] = FVector2D(0, 0);
+    UV[I + 1] = FVector2D(0, 1);
+    UV[I + 2] = FVector2D(1, 1);
+
+    Tangents[I].TangentX = (Triangles[I + 1] - Triangles[I]).GetSafeNormal();
+    Tangents[I + 1].TangentX = (Triangles[I + 2] - Triangles[I + 1]).GetSafeNormal();
+    Tangents[I + 2].TangentX = (Triangles[I] - Triangles[I + 2]).GetSafeNormal();
   }
 
   MeshComponent->bUseComplexAsSimpleCollision = true;
-  MeshComponent->CreateMeshSection_LinearColor(0, Triangles, VertexIndices, Normals, {}, {}, {}, true);
+  MeshComponent->CreateMeshSection_LinearColor(
+      0, 
+      Triangles, 
+      VertexIndices, 
+      Normals, 
+      UV, 
+      {}, 
+      Tangents, 
+      true);
   MeshComponent->ContainsPhysicsTriMeshData(true);
 }
