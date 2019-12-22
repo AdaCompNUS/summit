0bc22ca78e045aa15a0f4cf57e0ce5ed873aecc0
leeyiyuan <leeyiyuan@hotmail.com>
2019-12-22 14:01:13 +0800

    Cleanup and rearrangements

--- Scripts/calculate_occupancy.py
+++ Scripts/calculate_occupancy.py
@@ -3,7 +3,7 @@ import math
 import os
 import sys
 
-sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+sys.path.append(glob.glob('../PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
     sys.version_info.major,
     sys.version_info.minor,
     'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
